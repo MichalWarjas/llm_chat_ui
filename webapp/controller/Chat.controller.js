51db3f407cda4312bd521d748b3680d5dcaa6e50
Michał Borbant <birbanciak@proton.me>
2024-05-03 19:52:47 +0200

    adding llm api call

--- webapp/controller/Chat.controller.js
+++ webapp/controller/Chat.controller.js
@@ -13,12 +13,28 @@ sap.ui.define([
                 var oModel = new JSONModel();
                 this.getView().setModel(oModel);
             },
+            fetchExternalData: async function(question) {
+                try {
+                  const response = await fetch("http://127.0.0.1:8000/generate",{
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(question),
+                  });
+                  const data = await response.json();
+                  console.log(data);
+                } catch (error) {
+                  console.error('Failed to fetch external data', error);
+                }
+              },           
             onPost: function(oEvent) {
                 var oFormat = DateFormat.getDateTimeInstance({ style: "medium" });
                 var oDate = new Date();
                 var sDate = oFormat.format(oDate);
                 // create new entry
                 var sValue = oEvent.getParameter("value");
+                this.fetchExternalData(sValue);
                 var oEntry = {
                     Author: "Alexandrina Victoria",
                     AuthorPicUrl: "http://upload.wikimedia.org/wikipedia/commons/a/aa/Dronning_victoria.jpg",
@@ -30,7 +46,11 @@ sap.ui.define([
                 // update model
                 var oModel = this.getView().getModel();
                 var aEntries = oModel.getData().EntryCollection;
+                if(aEntries && aEntries.length > 0){
                 aEntries.unshift(oEntry);
+                }else{
+                aEntries = [oEntry];
+                }
                 oModel.setData({
                     EntryCollection: aEntries
                 });
